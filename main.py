25ea8f7dea4f96ca50f85b36e499d7fc7e190756
Calum Andrew Morrell <calum@drulum.com>
2021-04-10 01:52:59 +0100

    converted static script to class to manage console app

--- main.py
+++ main.py
@@ -76,12 +76,6 @@ class OneShot:
         self.confirm_files_exist()
         self.collect_message_parts()
 
-    def preview(self):
-        self.construct()
-        print(f'\nEmails will be sent from "{self.email_from}".')
-        print(f'The subject line is "{self.email_subject}"')
-        print(f'The message body is:\n{self.email_message}')
-
     def simple_test_run(self):
         """Runs through a full email construction & send test, but prints to a console using a local debugging smtp."""
         self.construct()
@@ -93,39 +87,59 @@ class OneShot:
         self.email_run()
 
 
-def display_menu():
-    print('\nOne Shot Mail')
-    print('=============\n')
-    print('1. Preview the email that will be sent.')
-    print('2. Trial run with files in the email preparation sub directory.')
-    print('3. LIVE RUN with files in the email preparation sub directory.')
-    print('0. Exit One Shot Mail.\n')
+class OneShotConsole:
+
+    def __init__(self):
+        self.choice = None
+
+    def clear_screen(self):
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+    def process_menu(self):
+        self.clear_screen()
+        print('One Shot Mail')
+        print('=============\n')
+        print('1. Preview the email that will be sent.')
+        print('2. Trial run with files in the email preparation sub directory.')
+        print('3. LIVE RUN with files in the email preparation sub directory.')
+        print('0. Exit One Shot Mail.\n')
+        self.choice = input('Enter option: ')
+
+    def run(self):
+        one_shot = OneShot()
+        while True:
+            self.process_menu()
+            if self.choice == '1':
+                one_shot.construct()
+                print(f'\nEmails will be sent from "{one_shot.email_from}".')
+                print(f'\nThe subject line is "{one_shot.email_subject}"')
+                print(f'\nThe message body is:\n{one_shot.email_message}')
+                input('\nPress <return> to continue.')
+            elif self.choice == '2':
+                one_shot.simple_test_run()
+                input('\nPress <return> to continue.')
+            elif self.choice == '3':
+                print('\nOne Shot Mail will exit once all emails have been sent.\n')
+                one_shot.simple_send()
+                break
+            elif self.choice == '0':
+                print('\nGoodbye!')
+                quit()
+            else:
+                print('\nI did not recognise your selection. Please ensure you enter an option from the menu.')
+                input('\nPress <return> to continue.')
 
 
 if __name__ == '__main__':
     try:
-        one_shot = OneShot()
-        display_menu()
-        choice = input('Enter option: ')
-        while True:
-            if choice == '1':
-                one_shot.preview()
-                display_menu()
-                choice = input('Enter option: ')
-            elif choice == '2':
-                one_shot.simple_test_run()
-                break
-            elif choice == '3':
-                one_shot.simple_send()
-                break
-            elif choice == '0':
-                print('\nGoodbye!')
-                quit()
-            else:
-                choice = input('\nPlease ensure you select an option from the list: ')
+        console = OneShotConsole()
+        console.run()
     except FileNotFoundError as error:
         print(f'\n{error}')
         print('Please run the application again once this has been corrected.')
         quit()
 
-    print('\nRun complete. All emails sent successfully.\nGoodbye!')
+    print('\nRun complete. All emails sent successfully.\n\nGoodbye!')
