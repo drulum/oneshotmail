e6441a591f1b963730f076dba9a6fa33cbc3cc42
Calum Andrew Morrell <calum@drulum.com>
2021-04-10 23:43:10 +0100

    configuring project for future packaging

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oneshotmail-drulum",
+    version="0.2",
+    author="Calum Andrew Morrell",
+    author_email="calum@drulum.com",
+    description="A simple mailshot package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drulum/oneshotmail",
+    project_urls={
+        "Bug Tracker": "https://github.com/drulum/oneshotmail/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
