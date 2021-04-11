f92f9af7c770b4d40d74978616331346ca9b97a9
Calum Andrew Morrell <calum@drulum.com>
2021-04-12 00:17:44 +0100

    installed packaged locally for testing

--- setup.py
+++ setup.py
@@ -22,5 +22,9 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'pytest',
+        'python-dotenv',
+    ],
     python_requires=">=3.6",
 )
