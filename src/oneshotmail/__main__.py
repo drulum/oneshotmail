66fdb1e7f155e7222388326625ba78bba755b2a1
Calum Andrew Morrell <calum@drulum.com>
2021-04-12 23:02:14 +0100

    handling missing files in code, removing exception catch

--- src/oneshotmail/__main__.py
+++ src/oneshotmail/__main__.py
@@ -4,10 +4,6 @@ from oneshotmail.console import OneShotConsole
 try:
     console = OneShotConsole()
     console.run()
-except FileNotFoundError as error:
-    print(f'\n{error}')
-    print('Please run the application again once this has been corrected.')
-    sys.exit()
 except ConnectionRefusedError as error:
     print(f'\n{error}')
     print('Please run the application again once this has been corrected.')
